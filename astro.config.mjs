f7b23ab51dee949d6d1dfb5c3043923457fb75bb
Junkai Ji <98195504+swampholyten@users.noreply.github.com>
2025-04-10 14:05:03 +0200

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,63 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightSidebarTopics from "starlight-sidebar-topics";
 
-// https://astro.build/config
+import starlightThemeRapidePlugin from "starlight-theme-rapide";
+
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      customCss: ["@/styles/global.css"],
+      plugins: [
+        starlightThemeRapidePlugin(),
+
+        starlightSidebarTopics([
+          {
+            label: "魏书",
+            link: "/wei/",
+            icon: "open-book",
+            items: Array.from({ length: 2 }, (_, i) => `wei/${i + 1}`),
+            badge: {
+              text: "更新中",
+              variant: "caution",
+            },
+          },
+          {
+            label: "蜀书",
+            link: "/shu/",
+            icon: "information",
+            items: Array.from({ length: 15 }, (_, i) => `shu/${i + 1}`),
+            badge: {
+              text: "完成",
+              variant: "success",
+            },
+          },
+          {
+            label: "吴书",
+            link: "/wu/",
+            icon: "information",
+            items: Array.from({ length: 2 }, (_, i) => `wu/${i + 1}`),
+            badge: {
+              text: "更新中",
+              variant: "caution",
+            },
+          },
+        ]),
+      ],
+      locales: {
+        root: {
+          label: "中文",
+          lang: "zh-CN",
+        },
+      },
+      title: "三国志",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/swampholyten",
+        },
+      ],
+    }),
+  ],
 });
